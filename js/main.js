a9d632192dccf58316478b596899fe18f911e443
loris.guelat <loris.guelat@divcom.ch>
2023-02-08 09:13:13 +0100

    demo prompt et conversion celcius

--- js/main.js
+++ js/main.js
@@ -1,55 +1,68 @@
 "use strict";
-// alert("Bonjour le monde");
-let nom = "Loris";
 
+let celcius = Number(prompt("Entrez la température en °C"));
+
+let fahrenheit = celcius *9 /5 +32;
+
+if(isNaN(celcius)){
+    alert("Entrez un nombre");
+}
+else {
+    alert(`${celcius}°C => ${fahrenheit}°F`);
+}
+
+// Récupérer le prénnom du visiteur
+const prenom = prompt("Quel est ton prénom ?");
+alert("Bienvenu " + prenom);
+
+// alert("Bonjour le monde !");
+let nom = "Guelat";
 // Affiche la variable dans la console
-console.log("Salut, le ", nom);
-
-// récupère le <h1> de la page
+console.log("Salut"," le ",nom);
+// récupère le premier <h1> de la page
 const titrePrincipal = document.querySelector("h1");
-
 // titre en jaune
 titrePrincipal.style.color = "yellow";
 
-
 /**
- * Ajoute du style à tous les li de la page
+ * Ajoute du style à tous les `<li>` de la page
  */
-function styleLi() {
-// Récupère tous les <li> dans un tableau
+function styleLi () {
+    // Récupère tous les <li> dans un tableau
     const elementsDeListe = document.querySelectorAll("li");
     console.log(elementsDeListe);
-
-// Mets en rouge le 2e élément trouvé
+    // Mets en rouge le 2e élément trouvé
     elementsDeListe[1].style.color = "red";
-
-// Boucle qui parcourt et mets un fond jaune à tous les éléments
+    // Boucle qui parcourt et mets un fond jaune à tous les éléments
     for (let i = 0; i < elementsDeListe.length; i++) {
         elementsDeListe[i].style.backgroundColor = "yellow";
     }
-
+    // Même chose avec for..of pour mettre en le texte en gras
     for (let element of elementsDeListe) {
         element.style.fontWeight = "bold";
     }
 }
 
-
-// Ecouter l'événement clic sur le titre principale <h1>
+// Ecouter l'événement click sur le <h1>
 titrePrincipal.addEventListener("click", styleLi);
-
-// Ecouter l'envoi du formulaire
+// récupère le formulaire
 const form = document.querySelector("form");
 // récupère la <ol> avec la classe commentaires
-const olCommentaires= document.querySelector(".commentaires");
-// récupère l'input avec l'id commentaire
-const inputCommentaire = document.querySelector("#commentaire");
-
-form.addEventListener('submit', function ajouterCommentaire(event){
+const olCommentaires = document.querySelector(".commentaires");
+// récupère l'input #commentaire
+const inpnutCommenaire = document.querySelector("#commentaire");
+// Ecouter l'envoi du formulaire
+form.addEventListener("submit", function ajouterCommentaire(event){
     event.preventDefault(); // Stoppe l'envoi du formulaire
+    // Si commentaire vide, avertir utilisateur
+    if (inpnutCommenaire.value === "") {
+        alert("Commentaire vide !");
+        return; // Stoppe l'événement
+    }
     // ajoute le contenu du champ commentaire à la liste
-    olCommentaires.innerHTML += "<li>" + inputCommentaire.value + "</li>";
+    olCommentaires.innerHTML += "<li>" + inpnutCommenaire.value + "</li>";
     // Vide le champ commentaire
-    inputCommentaire.value = "";
-    // Redone le focus au champ commentaire
-    inputCommentaire.focus();
+    inpnutCommenaire.value = "";
+    // Redonne le focus au champ commentaire
+    inpnutCommenaire.focus();
 });
